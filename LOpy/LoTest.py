407f005b4af97d27b53b34c5f5491edc127d11c0
martrewes <martinweston90@gmail.com>
2021-04-17 21:51:47 +0100

    Wrote the bulk of the 'engine' to respond to user input

--- LOpy/LoTest.py
+++ LOpy/LoTest.py
@@ -1,30 +1,103 @@
 # selection = input()
-
-grid = [[0,0,1,0,0],[0,1,1,1,0],[0,0,1,0,0],[0,0,0,0,0],[0,0,1,1,0]]
-
+moves = 0
 rowX = ""
-gridX, gridY = input('Enter location:').split()
 
+def ReadInput(moves):
+    gridX, gridY = input('Enter location:').split()
+    if gridX == 'a':
+        gridX = 0
+    elif gridX == 'b':
+        gridX = 1
+    elif gridX == 'c':
+        gridX = 2
+    elif gridX == 'd':
+        gridX = 3
+    elif gridX == 'e':
+        gridX = 4
+    gridY = int(gridY) - 1
+    moves += 1
+    return gridX, gridY, moves
 
-int = 1
-print('X Value:', gridX)
-print('Y Value:', gridY)
-print(' : A : B : C : D : E :')
-for row in grid:
-    
-    rowX = " |"
-    for each in row:
-        if each == 0:
-            rowX = rowX + "   |"
+def PrintBoard(grid):
+    int = 1
+    print(' : A : B : C : D : E :')
+    for row in grid:
+
+        rowX = " |"
+        for each in row:
+            if each == 0:
+                rowX = rowX + "   |"
+            else:
+                rowX = rowX + "XXX|"
+        if int <= 5:
+            print(str(int) + '+---+---+---+---+---+')
         else:
-            rowX = rowX + "XXX|"
-    if int <= 5:
-        print(str(int) + '+---+---+---+---+---+')
-    else:
-        print(' +---+---+---+---+---+')
-    print(rowX)
-    print(rowX)
-    rowX = ""
-    int += 1
+            print(' +---+---+---+---+---+')
+        print(rowX)
+        print(rowX)
+        rowX = ""
+        int += 1
 
-print(' +---+---+---+---+---+')
+    print(' +---+---+---+---+---+')
+
+def Move(gridX, gridY):
+    #if gridX > 0:
+    
+    if grid[gridY][gridX] == 0:
+        grid[gridY][gridX] = 1
+    else:
+        grid[gridY][gridX] = 0
+
+    if gridX > 0:
+        if grid[gridY][gridX - 1] == 0:
+            grid[gridY][gridX - 1] = 1
+        else:
+            grid[gridY][gridX - 1] = 0
+    
+    if gridX < 4:
+        if grid[gridY][gridX + 1] == 0:
+            grid[gridY][gridX + 1] = 1
+        else:
+            grid[gridY][gridX + 1] = 0
+    
+    if gridY > 0:
+        if grid[gridY - 1][gridX] == 0:
+            grid[gridY - 1][gridX] = 1
+        else:
+            grid[gridY - 1][gridX] = 0
+    
+    if gridY < 4:
+        if grid[gridY + 1][gridX] == 0:
+            grid[gridY + 1][gridX] = 1
+        else:
+            grid[gridY + 1][gridX] = 0
+    
+
+# Define main method that calls other functions
+def main(grid):
+    moves = 0
+    LevelComplete = False
+    print(sum(map(sum, grid)))
+    PrintBoard(grid)
+    while LevelComplete == False:
+        if (sum(map(sum, grid))) >= 1:
+
+            gridX, gridY, moves = ReadInput(moves)
+            Move(gridX, gridY)
+            print(gridX, gridY)
+            print('Moves: ' + str(moves))
+            PrintBoard(grid)
+            print(grid[gridY][gridX])
+            print(grid[0][1])
+            
+        else:
+            LevelComplete = True
+            print('OUTAHEAR')
+        
+
+
+
+# Execute main() function
+if __name__ == '__main__':
+    grid = [[0,0,1,0,0],[0,1,1,1,0],[0,0,1,0,0],[1,0,1,0,1],[1,0,1,0,1]]
+    main(grid)
