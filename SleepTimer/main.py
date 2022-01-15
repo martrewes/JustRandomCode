167a2f50817a2de0e7bee1a934ecd3df8e4dc71a
Martin <martinweston90@gmail.com>
2022-01-15 17:20:17 +0000

    Moved the shutdown routines into their own functions.

--- SleepTimer/main.py
+++ SleepTimer/main.py
@@ -2,6 +2,7 @@ from PyQt5 import QtWidgets, uic, QtCore
 import sys
 import os
 
+#Check if --lodpi was an arguement, if not use HiDPI
 if len(sys.argv) == 2 and sys.argv[1] == '--lodpi':
     print("Using Low DPI mode")
 else:
@@ -13,6 +14,7 @@ else:
 
 class Ui(QtWidgets.QMainWindow):
     
+    #Pull in all of the interface elements so I can interact with them
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('SleepTimer/main.ui', self)
@@ -37,18 +39,20 @@ class Ui(QtWidgets.QMainWindow):
         self.active = False
         self.show()
         
+    #Update the minute text if the dial is moved    
     def sliderMoved(self):
         self.spnTime.setValue(int(self.dial.value()))
     
+    #Use buttons on each side for fine control
     def btnChangeMinsUp(self):
         self.spnTime.setValue(self.spnTime.value() + 1)
     
     def btnChangeMinsDown(self):
         self.spnTime.setValue(self.spnTime.value() - 1)
 
+    # Button press routine
     def btnPressed(self):
         global timeSet 
-        
         
         if self.active == False:
             timeSet = int(self.spnTime.value())
@@ -60,7 +64,7 @@ class Ui(QtWidgets.QMainWindow):
             self.timer0 = QtCore.QTimer()
             self.time = QtCore.QTime(0, 0, 0)
             self.time = self.time.addSecs(int(self.spnTime.value()) * 60)
-            self.timer0.setInterval(1000)
+            self.timer0.setInterval(50)
             self.timer0.timeout.connect(lambda:self.ticker())
             self.timer0.start()
         else:
@@ -72,40 +76,50 @@ class Ui(QtWidgets.QMainWindow):
             self.statusBar.clearMessage()
             self.spnTime.setValue(timeSet)
 
+    #Make dial follow the minute text
     def timeChanged(self):
         self.dial.setValue(self.spnTime.value())
-    def osShutdown():
-        print(os.name)
+
+    #define shutdown types
+    def osShutdown(self):
+        print("Shutdown!")
+        if os.name == 'nt':
+            os.system("shutdown /s /t 10")
+        else: 
+            os.system("systemctl poweroff")
+    def osRestart(self):
+        print("Restarting!")
+        if os.name == 'nt':
+            os.system("shutdown /r /t 10")
+        else:
+            os.system("systemctl reboot")
+    def osSleep(self):
+        print("Sleeping!")
+        if os.name == 'nt':
+            os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
+        else:
+            os.system("systemctl suspend")
+    def osHibernate(self):
+        print("Hibernating!")
+        if os.name == 'nt':
+            os.system("rundll32.exe powrprof.dll,SetSuspendState 1,1,0")
+        else:
+            os.system("systemctl hibernate")
+    
+    # on each tick, what to do
     def ticker(self):
         if self.active == True:
             global time
             self.spnTime.setValue((int(self.time.toString("hh")) * 60) + int(self.time.toString("mm")))
             if self.time.second() == 0 and self.time.minute() == 0 and self.time.hour() == 0:
                 if self.radShutdown.isChecked():
-                    #print("Shutdown!")
-                    #if os.name == 'nt':
-                    #    os.system("shutdown /s /t 10")
-                    #else: 
-                    #    os.system("systemctl poweroff")
-                    osShutdown()
+                    self.osShutdown()
                 if self.radRestart.isChecked():
-                    print("Restarting!")
-                    if os.name == 'nt':
-                        os.system("shutdown /r /t 10")
-                    else:
-                        os.system("systemctl reboot")
+                    self.osRestart()
                 if self.radSleep.isChecked():
-                    print("Sleeping!")
-                    if os.name == 'nt':
-                        os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
-                    else:
-                        os.system("systemctl suspend")
+                    self.osSleep()
                 if self.radHibernate.isChecked():
-                    print("Hibernating!")
-                    if os.name == 'nt':
-                        os.system("rundll32.exe powrprof.dll,SetSuspendState 1,1,0")
-                    else:
-                        os.system("systemctl hibernate")
+                    self.osHibernate()
                 exit()
             self.time = self.time.addSecs(-1)
             self.statusBar.showMessage("Time Remaining: " + self.time.toString("hh:mm:ss",))
